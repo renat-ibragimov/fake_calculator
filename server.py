28a11af101ed43c6137c13a8a299b9ae22ea1308
renat_ibragimov <r.ibragimov@iconic.info>
2025-04-18 12:03:55 +0300

    Increase loop interval

--- server.py
+++ server.py
@@ -68,7 +68,7 @@ async def increase_counter() -> None:
 async def update_loop() -> None:
     print("🕒 Update loop started", flush=True)
     while True:
-        interval = random.randint(60, 120)
+        interval = random.randint(1800, 5400)
         print(f"⏳ Sleeping for {interval // 60} min...", flush=True)
         await asyncio.sleep(interval)
         await increase_counter()
